b8c36cfe2c71e7be41e75ab6f066dbbd2cdca642
carles-garcia <>
2016-01-26 17:40:17 +0100

    option parsing started

--- main.c
+++ main.c
@@ -3,6 +3,56 @@
 #include <argp.h>
 #include "log_parse.h"
 
+
+const char *argp_program_version = "aptback v0.1";
+const char *argp_program_bug_address = "https://github.com/carles-garcia/aptback/issues";
+static char doc[] = 
+"aptback -- a utility to search, install and remove packages logged by apt";
+
+static char args_doc[] = "install|remove|upgrade|search";
+
+static struct argp_option options[] = {
+  {"date"	'd', "DATE", 0, "Select packages in a date" },
+  {"until",	'u', "DATE", 0, "If date option specified, select packages from the range date:until (both included)" },
+  {"option",   	'o', "OPTIONS", 0, "Select packages that were installed, removed and/or upgraded" },
+  { 0 }
+};
+
+struct arguments {
+  char *date, *until, *options, *action;
+};
+
+/* aptback remove --option installed,updated --date 2015-11-09-17-54-22 --until 2015-12-19
+ * date format: 2015-11-09-17-54-22
+ * minimum input is year. Then others wil be auto completed.*/
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+  struct arguments *arguments = state->input;
+  switch (key)
+  {
+    case 'd':
+      arguments->date = arg;
+      break;
+    case 'u':
+      arguments->until = arg;
+      break;
+    case 'o':
+      arguments->options = arg;
+      break;
+    case ARGP_KEY_ARG:
+      arguments->action = arg;
+      break;
+    case ARGP_KEY_END:
+      if (state->arg_num < 1) argp_usage(state);
+      arguments->arg_num = state->arg_num;
+      break;
+    default:
+      return ARGP_ERR_UNKNOWN;
+  return 0;
+}
+
+static struct argp argp = {options, parse_opt, args_doc, doc};
+
 int main() {
   
   char *filename = "ignore/hist.txt";
