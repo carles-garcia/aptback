45486827b13c8e447c1866df59be8f4aaabe83b6
carles-garcia <>
2016-02-02 20:39:35 +0100

    changed print format

--- print_search.c
+++ print_search.c
@@ -5,13 +5,13 @@ void print_search(struct darray *selected) {
     struct action *a = darray_get(selected, i);
     for (int j = 0; j < a->packages.size; ++j) {
       struct package *p = darray_pack_get(&a->packages, j);
-      printf("%d-%02d-%02d %02d:%02d ", a->date.year, a->date.month, a->date.day, a->date.hour, a->date.minute);
-      if (a->type == INSTALL) printf("install ");
-      else if (a->type == REMOVE) printf("remove ");
-      else printf("upgrade ");
-      printf("%s %s %s ", p->name, p->arch, p->version);
-      if (p->automatic) printf("automatic");
-      else if (p->newversion != NULL) printf(" -> %s", p->newversion);
+      printf("%d-%02d-%02d  %02d:%02d", a->date.year, a->date.month, a->date.day, a->date.hour, a->date.minute);
+      if (a->type == INSTALL) printf("  install");
+      else if (a->type == REMOVE) printf("  remove");
+      else printf("  upgrade");
+      printf("    %s  %s  (%s)", p->name, p->arch, p->version);
+      if (p->automatic) printf("  automatic");
+      else if (p->newversion != NULL) printf("-->(%s)", p->newversion);
       printf("\n");
     }
   }
